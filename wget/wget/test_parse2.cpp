dc4db9ca9c8dad595b2ca8b170ea6bb339213d4e
NDeyneka <45417642+NDeyneka@users.noreply.github.com>
2019-04-19 16:42:14 +0500

    Update test_parse2.cpp

--- wget/wget/test_parse2.cpp
+++ wget/wget/test_parse2.cpp
@@ -45,78 +45,6 @@ void parse_link_r(vector<string> &link_list, string string_to_parse)
 }
 
 
-void parse_img_link_r(vector<string> &link_list, string string_to_parse)
-{
-
-	const char *rg = R"("(https{0,1}:/){0,1}/+.*")";
-
-	const char *rg_h = R"(<img\s*[^>]*src\s*=[^>]*>)";
-
-
-	regex http_regex(rg_h);
-	regex link_regex(rg);
-
-
-	smatch http_match;
-	smatch link_match;
-
-	string tmp;
-
-	string tmps = string_to_parse;
-
-	while (regex_search(tmps, http_match, http_regex))
-	{
-		tmp = http_match.str();
-
-		//cout << tmp << "\n";
-
-		tmps = http_match.suffix();
-
-		if (regex_search(tmp, link_match, link_regex))
-		{
-			link_list.push_back(link_match.str());
-		}
-
-	}
-
-}
-
-void parse_link(vector<string> &link_list, string string_to_parse)
-{
-	regex http_regex("<a.*=.*\"https{0,1}://.*(?=\"\>)");
-	
-	//regex http_regex("<a.*href.*=.*(?=\"\>)");
-
-	//regex link_regex("(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?");
-
-	regex link_regex("https{0,1}://.*");
-
-	smatch http_match;
-	smatch link_match;
-
-	string tmp;
-
-	if (regex_search(string_to_parse, http_match, http_regex)) {
-
-		for (size_t i = 0; i < http_match.size(); ++i)
-		{
-			tmp = http_match[i].str();
-			if (regex_search(tmp, link_match, link_regex))
-			{
-				for (size_t j = 0; j < link_match.size(); ++j)
-				{
-					link_list.push_back(link_match[j]);
-				}
-			}
-
-
-		}
-	}
-
-
-}
-
-
 void parse_hostname(string addr, string &protocol, string &hostname, string &tail)
 {
 	regex prot_regex("https{0,1}");
@@ -142,12 +70,10 @@ void parse_hostname(string addr, string &protocol, string &hostname, string &tai
 
 	regex_search(tmp, match, host2_regex);
 
-	hostname = match.prefix();  //òóò íàäî óáèðàòü www èëè íåò?
+	hostname = match.prefix();  //Ã²Ã³Ã² Ã­Ã Ã¤Ã® Ã³Ã¡Ã¨Ã°Ã Ã²Ã¼ www Ã¨Ã«Ã¨ Ã­Ã¥Ã²?
 	tail = match.suffix();
 	
 }
-
-
 
 
 void parse_error(string filename, string &error, int& err_num, string &location)
@@ -206,7 +132,6 @@ void parse_error(string filename, string &error, int& err_num, string &location)
 
 }
 
-
 void parse_img_link(vector<string> &link_list, string string_to_parse)
 {
 	regex http_regex("<img.*src.*=.*\".*(?=\" )");
@@ -246,66 +171,3 @@ void parse_img_link(vector<string> &link_list, string string_to_parse)
 }
 
 
-int main()
-{
-	string lines[] = { "<a class=gb1 href=\"http://www.google.co.uz/imghp?hl=uz&tab=wi\">Rasmlar</a>",
-		"<a class = gb1 href = \"https://mail.google.com/mail/?tab=wm\">Gmail< / a>",
-		"<a class = gb1 href = \"https://drive.google.com/?tab=wo\">Drive< / a>",
-		"< class = gb1 href = \"htt://www.google.com/calendar?tab=wc\">Taqvim< / a>" };
-
-	string test_line = "<a class=gb1 href=\"http://www.google.co/\">Rasmlar</a>, next <a href=\"https://mail.ru\">Gmail< / a>";
-
-	string img_line = "<img src = \"/html/images/test.png\" alt = \"Simply Easy Learning\"";
-	//find <a href = .... 
-
-	regex http_regex("(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?");
-
-	regex link_regex("https{0,1}://.*");
-
-
-	vector<string> link_list;
-	string protocol, hostname, tail;
-
-
-	parse_img_link(link_list, img_line);
-
-	for (size_t i = 0; i < link_list.size(); i++)
-	{
-		cout << i << ": " << link_list[i] << "\n\n";
-	}
-
-	/*for (const auto& line : lines) {
-		parse_link(link_list, line);
-	}
-
-	parse_hostname(link_list[0], protocol, hostname, tail);
-
-	for (size_t i = 0; i < link_list.size(); i++)
-	{
-		cout << i << ": " << link_list[i] << "\n\n";
-	}
-
-	for (size_t i = 0; i < link_list.size(); i++)
-	{
-		parse_hostname(link_list[i], protocol, hostname, tail);
-
-		cout << i << " : " << protocol << " : " << hostname << " : " << tail << "\n\n";
-	}*/
-
-
-	/*string error;
-	int err_num; 
-	string location;
-
-	parse_error("wwwunguz.html", error, err_num, location);
-
-	cout << error << " " << err_num << " " << location << "\n";
-
-	parse_error("in.txt", error, err_num, location);
-
-	cout << error << " " << err_num << " " << location << "\n\n";
-
-	parse_img_link(link_list, img_line);
-
-	cout << link_list[link_list.size()-1] << "\n";*/
-}
